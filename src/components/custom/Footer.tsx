a6e7ddaf2328d4c31dc4ae72a9c2c406b63ad1e3
Nishant4coding <srivastava4nishant@gmail.com>
2025-01-17 17:47:20 +0530

    footer updates #2

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -1,50 +1,49 @@
-import { Linkedin } from "lucide-react";
+import { FaLinkedin } from "react-icons/fa6";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-[#d6ceb3] border-t border-gray-300">
-      <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between py-4 px-6">
-        {/* Logo and Copyright */}
-        <div className="flex items-center space-x-2">
+    <footer className="bg-[#d6ceb3] border-t border-gray-300  px-6 lg:px-24">
+      <div className=" mx-auto flex flex-col sm:flex-row items-center justify-between py-4">
+        <div className="flex items-center space-x-2 lg:flex-row flex-col">
           <Image
-            src="/logo.png" // Replace with your logo path
+            src="/logo2.png"
             alt="Logo"
-            className="h-6"
-            width={50}
+            className="w-72 "
+            width={200}
             height={50}
           />
-          <span className="text-sm text-gray-800">
+          <span className="text-md font-semibold text-gray-800">
             &copy; 2024 Columnsprout AI, Inc.
           </span>
         </div>
+        <div className="flex justify-end items-center lg:flex-row flex-col gap-2 lg:-gap-4">
+          <div className=" space-x-4 mt-4 sm:mt-0">
+            <Link
+              href="/privacy-policy"
+              className="text-md font-semibold text-gray-800 hover:underline"
+            >
+              Privacy Policy
+            </Link>
+            <Link
+              href="/terms-of-service"
+              className="text-md font-semibold text-gray-800 hover:underline"
+            >
+              Terms of Service
+            </Link>
+          </div>
 
-        {/* Navigation Links */}
-        <div className="flex space-x-4 mt-4 sm:mt-0">
-          <a
-            href="/privacy-policy"
-            className="text-sm text-gray-800 hover:underline"
-          >
-            Privacy Policy
-          </a>
-          <a
-            href="/terms-of-service"
-            className="text-sm text-gray-800 hover:underline"
-          >
-            Terms of Service
-          </a>
-        </div>
-
-        <div className="mt-4 sm:mt-0">
-          <Link
-            href="https://www.linkedin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Linkedin />
-          </Link>
+          <div className="mt-4 sm:mt-0">
+            <Link
+              href="https://www.linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin className="w-8 h-8" />
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
