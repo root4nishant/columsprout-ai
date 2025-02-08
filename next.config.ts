1bb60366d861d38121c085a7763e1ee7e0f46b29
Sarthak Bharadwaj <sarthak8bharadwaj@gmail.com>
2025-02-08 17:43:24 +0530

    Minor design changes and CSP changes

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self'; 
-              script-src 'self' 'unsafe-inline'; 
+              script-src 'self' 'unsafe-inline';
               style-src 'self' 'unsafe-inline'; 
               img-src 'self' https://storage.googleapis.com;
               media-src 'self' https://storage.googleapis.com;
