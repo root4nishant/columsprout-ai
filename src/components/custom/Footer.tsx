c4d60aaaec273788b2a35cf61f1b439b08233bd0
Sarthak Bharadwaj <sarthak8bharadwaj@gmail.com>
2025-02-08 20:34:41 +0530

    Footer changes for address

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -1,47 +1,67 @@
 import { FaLinkedin } from "react-icons/fa6";
+import { MapPin } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-[#d6ceb3] border-t border-gray-300 w-full mx-auto">
-      <div className=" mx-auto flex lg:max-w-[1400px] md:max-w-[900px] w-full px-6 lg:px-20 lg:flex-row md:flex-row flex-col items-center lg:justify-between justify-center py-4 space-y-3">
-        <div className="flex items-center lg:space-x-6 space-x-2 flex-row lg:w-full md:w-full">
-          <Image
-            loading="lazy"
-            src="/logo.svg"
-            alt="Logo"
-            className="w-10 "
-            width={20}
-            height={10}
-          />
-          <span className="text-xs lg:text-sm text-gray-800">
-            &copy; {new Date().getFullYear()} Columspro AI Inc.
-          </span>
-        </div>
-        <div className="flex md:w-full items-center lg:justify-end justify-between gap-10 lg:gap-8">
-          <div className="space-x-4 flex items-center lg:justify-end md:justify-end justify-between  text-left w-full">
+    <footer className="bg-[#d6ceb3] border-t border-gray-300 w-full">
+      <div className="mx-auto flex flex-col items-center max-w-[1400px] w-full px-4 lg:px-20 py-6 space-y-6">
+        {/* Top Section */}
+        <div className="flex flex-col items-center text-center sm:flex-row sm:justify-between w-full space-y-4 sm:space-y-0">
+          <div className="flex items-center justify-center space-x-4">
+            <Image
+              loading="lazy"
+              src="/logo.svg"
+              alt="Logo"
+              className="w-10"
+              width={20}
+              height={10}
+            />
+            <span className="text-xs sm:text-sm lg:text-sm text-gray-800">
+              &copy; {new Date().getFullYear()} Columspro AI Inc.
+            </span>
+          </div>
+          <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-4 space-y-2 sm:space-y-0">
             <Link
               href="/privacyPolicy"
-              className="lg:text-lg text-sm text-gray-800"
+              className="text-sm sm:text-base text-gray-800"
             >
               Privacy Policy
             </Link>
             <Link
               href="/termsAndService"
-              className="lg:text-lg text-sm  text-gray-800"
+              className="text-sm sm:text-base text-gray-800"
             >
               Terms of Service
             </Link>
-            <Link
-              href="https://www.linkedin.com/company/columsprout-ai/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin className="w-8 h-8" />
-            </Link>
+            <div className="flex justify-center w-full sm:w-auto">
+              <Link
+                href="https://www.linkedin.com/company/columsprout-ai/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin className="w-6 h-6 sm:w-8 sm:h-8 text-gray-800" />
+              </Link>
+            </div>
           </div>
+        </div>
+        {/* Divider Line */}
+        <div
+          className="w-[80%] sm:w-[70%] lg:w-[50%] mx-auto"
+          style={{
+            height: "1px",
+            backgroundColor: "#93905C",
+          }}
+        ></div>
+        {/* Address Section */}
+        <div className="flex flex-col sm:flex-row justify-center items-center sm:space-x-2 text-gray-800 text-center space-y-2 sm:space-y-0">
+          <MapPin className="w-6 h-6" />
+          <p className="text-sm lg:text-base sm:text-base text-[12px] sm:leading-normal leading-tight">
+            No 198, 2nd Floor, CMH Rd Stage 2, Indiranagar, Bangalore, Karnataka
+            560038
+          </p>
         </div>
       </div>
     </footer>
